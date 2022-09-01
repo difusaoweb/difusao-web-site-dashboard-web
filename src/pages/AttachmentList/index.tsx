fc286db6e8031bc479545379767108730c9df952
Difusão Web <difusaowebinc@gmail.com>
2022-09-01 10:23:44 -0300

    Page AttacmentList atualizado.

--- src/pages/AttachmentList/index.tsx
+++ src/pages/AttachmentList/index.tsx
@@ -1,64 +1,35 @@
 import * as React from 'react'
-import { Box, Button, Toolbar, Typography, Container } from '@mui/material'
+import { Box, Button, Typography } from '@mui/material'
 import { Add as AddIcon } from '@mui/icons-material'
 
-import { Copyright } from '../../components/atoms/Copyright'
-import { AppBar } from '../../components/atoms/AppBar'
-import { SideBar } from '../../components/molecules/SideBar'
+import styles from './index.module.scss'
+import { PageContainer } from '../../components/atoms/PageContainer'
 import { AttachmentList } from '../../components/ecosystems/AttachmentList'
 
 export const AttachmentListPage: React.FC = () => {
-  const [open, setOpen] = React.useState(true)
-  const toggleDrawer = () => {
-    setOpen(!open)
-  }
-
   return (
     <>
-      <Box sx={{ display: 'flex' }}>
-        <AppBar open={open} toggleDrawer={toggleDrawer} title="Adicionar" />
-        <SideBar open={open} toggleDrawer={toggleDrawer} />
-        <Box
-          component="main"
-          sx={{
-            backgroundColor: theme =>
-              theme.palette.mode === 'light'
-                ? theme.palette.grey[100]
-                : theme.palette.grey[900],
-            flexGrow: 1,
-            height: '100vh',
-            overflow: 'auto'
-          }}
-        >
-          <Toolbar />
-          <Container maxWidth="xl" sx={{ mt: 4, pb: 4 }}>
-            <Box
-              sx={{ display: 'flex', alignItens: 'center', marginBottom: 5 }}
-            >
-              <Box sx={{ flexGrow: 1 }}>
-                <Typography
-                  component="h1"
-                  variant="h5"
-                  sx={{ fontWeight: 'bold' }}
-                >
-                  Lista de mídias
-                </Typography>
-              </Box>
-              <Box sx={{ flexShrink: 0 }}>
-                <Button
-                  variant="contained"
-                  startIcon={<AddIcon />}
-                  href="/midias/adicionar/"
-                >
-                  Nova Mídia
-                </Button>
-              </Box>
+      <PageContainer>
+        <>
+          <Box className={styles.pageTopBar}>
+            <Box className={styles.box}>
+              <Typography component="h1" className={styles.titlePage}>
+                Lista de mídias
+              </Typography>
             </Box>
-            <AttachmentList />
-          </Container>
-          <Copyright />
-        </Box>
-      </Box>
+            <Box className={styles.box2}>
+              <Button
+                variant="contained"
+                startIcon={<AddIcon />}
+                href="/midias/adicionar"
+              >
+                Nova Mídia
+              </Button>
+            </Box>
+          </Box>
+          <AttachmentList />
+        </>
+      </PageContainer>
     </>
   )
 }
