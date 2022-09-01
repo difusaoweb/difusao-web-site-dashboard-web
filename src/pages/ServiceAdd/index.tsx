730761ed90265e9c2ec55d001386721deaff7d95
Difusão Web <difusaowebinc@gmail.com>
2022-09-01 15:29:55 -0300

    Page ServiceAdd atualizado.

--- src/pages/ServiceAdd/index.tsx
+++ src/pages/ServiceAdd/index.tsx
@@ -1,16 +1,8 @@
 import * as React from 'react'
-import {
-  Box,
-  Toolbar,
-  Typography,
-  Container,
-  Grid,
-  Button
-} from '@mui/material'
+import { Box, Typography, Grid, Button } from '@mui/material'
 
-import { Copyright } from '../../components/atoms/Copyright'
-import { AppBar } from '../../components/atoms/AppBar'
-import { SideBar } from '../../components/molecules/SideBar'
+import styles from './index.module.scss'
+import { PageContainer } from '../../components/atoms/PageContainer'
 import { ServicesAddFormEdit } from '../../components/ecosystems/ServicesAddFormEdit'
 import { ServicesAddFormEdit2 } from '../../components/ecosystems/ServicesAddFormEdit2'
 import {
@@ -22,11 +14,6 @@ import {
 export const ServiceAddPage: React.FC = () => {
   // const { createServiceReturn } = useAppSelector(state => state.services)
   const dispatch = useAppDispatch()
-
-  const [open, setOpen] = React.useState(true)
-  const toggleDrawer = () => {
-    setOpen(!open)
-  }
 
   const [title, setTitle] = React.useState<string | null>(null)
   const [description, setDescription] = React.useState<string | null>(null)
@@ -45,63 +32,38 @@ export const ServiceAddPage: React.FC = () => {
   }
 
   return (
-    <>
-      <Box sx={{ display: 'flex' }}>
-        <AppBar open={open} toggleDrawer={toggleDrawer} title="Adicionar" />
-        <SideBar open={open} toggleDrawer={toggleDrawer} />
-        <Box
-          component="main"
-          sx={{
-            backgroundColor: theme =>
-              theme.palette.mode === 'light'
-                ? theme.palette.grey[100]
-                : theme.palette.grey[900],
-            flexGrow: 1,
-            height: '100vh',
-            overflow: 'auto'
-          }}
-        >
-          <Toolbar />
-          <Container maxWidth="lg" sx={{ mt: 4, pb: 4 }}>
-            <Box
-              sx={{ display: 'flex', alignItens: 'center', marginBottom: 5 }}
-            >
-              <Box sx={{ flexGrow: 1 }}>
-                <Typography
-                  component="h1"
-                  variant="h5"
-                  sx={{ fontWeight: 'bold' }}
-                >
-                  Criar um novo servico
-                </Typography>
-              </Box>
-            </Box>
-            <Grid container spacing={3}>
-              <Grid item xs={8}>
-                <ServicesAddFormEdit
-                  title={title}
-                  setTitle={setTitle}
-                  description={description}
-                  setDescription={setDescription}
-                />
-              </Grid>
-              <Grid item xs={4}>
-                <ServicesAddFormEdit2 image={image} setImage={setImage} />
-                <Button
-                  variant="contained"
-                  size="large"
-                  fullWidth
-                  onClick={createService}
-                  disabled={!title || !description || !image}
-                >
-                  Criar servico
-                </Button>
-              </Grid>
-            </Grid>
-          </Container>
-          <Copyright />
+    <PageContainer title="Adicionar">
+      <>
+        <Box className={styles.pageTopBar}>
+          <Box className={styles.box}>
+            <Typography component="h1" className={styles.titlePage}>
+              Criar um novo servico
+            </Typography>
+          </Box>
         </Box>
-      </Box>
-    </>
+        <Grid container spacing={3}>
+          <Grid item xs={8}>
+            <ServicesAddFormEdit
+              title={title}
+              setTitle={setTitle}
+              description={description}
+              setDescription={setDescription}
+            />
+          </Grid>
+          <Grid item xs={4}>
+            <ServicesAddFormEdit2 image={image} setImage={setImage} />
+            <Button
+              variant="contained"
+              size="large"
+              fullWidth
+              onClick={createService}
+              disabled={!title || !description || !image}
+            >
+              Criar servico
+            </Button>
+          </Grid>
+        </Grid>
+      </>
+    </PageContainer>
   )
 }
