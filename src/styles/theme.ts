c927bfef77a74bda62cee913a1c241b9f2f7b3e8
Difusão Web <difusaowebinc@gmail.com>
2022-09-09 10:10:41 -0300

    Theme update.

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,7 +1,6 @@
-import { createTheme } from '@mui/material/styles'
-import { red } from '@mui/material/colors'
+import { createTheme, ComponentsOverrides } from '@mui/material/styles'
 
-export const theme = createTheme({
+const themeConst = {
   palette: {
     mode: 'dark',
     primary: {
@@ -26,6 +25,43 @@ export const theme = createTheme({
     background: {
       default: '#181924',
       paper: '#24252f'
+    }
+  }
+}
+
+export const theme = createTheme({
+  palette: {
+    mode: themeConst.palette.mode,
+    primary: {
+      main: themeConst.palette.primary.main
+    },
+    secondary: {
+      main: themeConst.palette.secondary.main
+    },
+    success: {
+      main: themeConst.palette.success.main
+    },
+    error: {
+      main: themeConst.palette.error.main,
+      dark: themeConst.palette.error.dark
+    },
+    warning: {
+      main: themeConst.palette.warning.main
+    },
+    info: {
+      main: themeConst.palette.info.main
+    },
+    background: {
+      default: themeConst.palette.background.default,
+      paper: themeConst.palette.background.paper
+    }
+  },
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: `:root {
+        --palette-primary-main: ${themeConst.palette.primary.main};
+        --palette-error-main: ${themeConst.palette.error.main};
+      }`
     }
   }
 })
